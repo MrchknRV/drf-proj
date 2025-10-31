c09a5d2ab884deb5a3a3c9d9215b5dc8f0cb5594
mrchkn.r <rodionmarochkin32@gmail.com>
2025-11-01 00:54:29 +0300

    Реализовано поле вывода о количестве уроков в сериализаторе, добавлена новая модель Payment в приложении users и созданы фикстуры

--- materials/serializer.py
+++ materials/serializer.py
@@ -4,15 +4,19 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         # exclude = ("preview",)
-        fields = "__all__"
+        fields = ["name", "description", "lesson_count"]
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
+
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         # fields = ["name", "description"]
         fields = "__all__"
-
-
