1c57642cc888a6b8f27107a55e903ecf839439a4
mrchkn.r <rodionmarochkin32@gmail.com>
2025-11-03 22:18:26 +0300

    Реализован сериализатор для модели курса, который выводит количество уроков и информацию по всем урокам

--- materials/serializer.py
+++ materials/serializer.py
@@ -3,20 +3,21 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lesson_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        # exclude = ("preview",)
-        fields = ["name", "description", "lesson_count"]
-
-    def get_lesson_count(self, obj):
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         # fields = ["name", "description"]
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        # exclude = ("preview",)
+        fields = ["name", "description", "lesson_count", "lessons"]
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
